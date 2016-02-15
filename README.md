4b6916a6a2c7459f6f234d6a9a619862feada045
Chris Powell <powellchristoph@gmail.com>
2016-02-15 22:25:35 +0000

    Working boostrap.sh

--- README.md
+++ README.md
@@ -11,4 +11,16 @@
 - Please try to write close to production-quality code (whatever this means to you)
 - Please include instructions for how and where we should test your project.
 
+At the end of the Ansible run, it will print out the URL of the loadbalancer.
+```
+TASK [debug] *******************************************************************
+ok: [localhost] => {
+    "msg": "Site available at http://146.20.48.247"
+}
 
+PLAY RECAP *********************************************************************
+localhost                  : ok=3    changed=0    unreachable=0    failed=0
+web-iad-01                 : ok=17   changed=4    unreachable=0    failed=0
+web-iad-02                 : ok=17   changed=4    unreachable=0    failed=0
+web-iad-03                 : ok=17   changed=4    unreachable=0    failed=0
+```
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,120 @@
 #!/bin/bash
 
-# TODO
-# - write out given credentials to /root/.rackspace_cloud_credentials
+usage() {
+    echo "usage: bootstrap.sh -u rax_username -k rax_apikey [deploy | destroy]"
+}
 
-# create ssh keypair
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
-ssh-agent bash
-ssh-add ~/.ssh/id_rsa
+setup() {
+    ## create ssh keypair/ setup agent
+    if [[ ! -f ~/.ssh/id_rsa ]]; then
+        ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+    fi
+#    ssh-agent bash
+    ssh-add ~/.ssh/id_rsa
 
-# install ansible
-apt-get -y install software-properties-common python-dev python-pip curl
-apt-add-repository -y ppa:ansible/ansible
-apt-get update
-apt-get -y install ansible
+    # install some deps
+    apt-get -y install software-properties-common python-dev python-pip curl git
+    
+    # install ansible
+    apt-add-repository -y ppa:ansible/ansible
+    apt-get update
+    apt-get -y install ansible
+    rm -rf /etc/ansible/*
+    
+    # configure rs_module
+    pip install pyrax
+    # missing pyrax deps; https://github.com/rackspace/pyrax/issues/570
+    pip install wrapt
+    pip install monotonic
+    pip install netifaces
+    #curl https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/rax.py -o /etc/ansible/rax.py
+    #chmod +x /etc/ansible/rax.py
+    
+    # get repo
+    if [[ -d /tmp/devops_challenge ]]; then
+        rm -rf /tmp/devops_challenge
+    fi
+    git clone https://github.com/powellchristoph/devops_challenge.git /tmp/devops_challenge
+    cp -a /tmp/devops_challenge/ansible/* /etc/ansible/.
+}
 
-# NOTE: FOR DYNAMIC INVENTORY FROM CLOUD ACCOUNT. MIGHT NOT NEED IT.
-# configure rs_module
-pip install pyrax
-# missing pyrax deps; https://github.com/rackspace/pyrax/issues/570
-pip install wrapt
-pip install monotonic
-pip install netifaces
-curl https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/rax.py -o /etc/ansible/rax.py
-chmod +x /etc/ansible/rax.py
+deploy() {
+#    if ! which ansible >/dev/null; then
+#        echo "Cannot find ansible, did setup() run correctly?"
+#        exit 1
+#    fi
 
-# get repo
-# copy over /etc/ansible/ansible.cfg
-# copy over /etc/ansible/hosts
+    # bootstrap environment
+    ansible-playbook -i /etc/ansible/rax.py /etc/ansible/setup_site.yaml
+    if [[ $? -ne 0 ]]; then
+        echo "Something went wrong creating the environment."
+        exit 1
+    else
+        echo "Your environment is ready."
+    fi
+}
+
+destroy() {
+    ansible-playbook -i /etc/ansible/rax.py /etc/ansible/teardown_site.yaml
+    if [[ $? -ne 0 ]]; then
+        echo "Something went wrong destroying the environment."
+        exit 1
+    else
+        echo "Your environment is gone."
+    fi
+}
+
+function=0
+while [[ $# > 0 ]]; do
+    case $1 in
+        -u)
+            rax_username=$2
+            shift
+            ;;
+        -k)
+            rax_apikey=$2
+            shift
+            ;;
+        deploy)
+            function=1
+            ;;
+        destroy)
+            function=2
+            ;;
+        -h | --help)
+            usage
+            exit
+            ;;
+        *)
+            usage
+            exit 1
+    esac
+    shift
+done
+
+if [ -z $rax_username ] || [ -z $rax_apikey ]; then
+    usage
+    echo "No credentials given, quitting."
+    exit 1
+fi
+
+cat <<EOF > ~/.rackspace_cloud_credentials
+[rackspace_cloud]
+username = $rax_username
+api_key = $rax_apikey
+EOF
+
+case $function in
+    0)
+        usage
+        echo "No function given, quitting"
+        exit 1
+        ;;
+    1)
+        setup
+        deploy
+        ;;
+    2)
+        destroy
+        ;;
+esac
